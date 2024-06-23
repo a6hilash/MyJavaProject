ccc0e660ad855af7f01baf7fe38a9bcf97746a84
abhil <abhil@10.0.0.251>
2024-06-23 15:20:38 -0400

    Day5Loops

--- MyJavaProject/src/day5Loops/ForLoop.java
+++ MyJavaProject/src/day5Loops/ForLoop.java
@@ -13,11 +13,26 @@ public class ForLoop {
 		
 		//print even number
 		
-		for(int i=2;i<=10;i=i+2)
-		{
-			System.out.println(i);
-		}
+//		for(int i=2;i<=10;i=i+2)
+//		{
+//			System.out.println(i);
+//		}
 
+		
+//		for(int i=1;i<=10;i++)
+//		{
+//			if(i%2 == 0)
+//			{
+//				System.out.println(i);
+//			}
+//		}
+		
+		//print numbers from 10 to 1
+		
+//		for(int i=10;i>=1;i--)
+//		{
+//			System.out.println(i);
+//		}
 	}
 
 }
