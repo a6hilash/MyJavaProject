83aa8da7d749cd78d16cabc51a6c0d290b2344bc
abhil <abhil@10.0.0.251>
2024-06-23 15:38:47 -0400

    Day5loops

--- MyJavaProject/src/day5Loops/JumpStatements.java
+++ MyJavaProject/src/day5Loops/JumpStatements.java
@@ -12,9 +12,18 @@ public class JumpStatements {
 //			System.out.println(i);
 //		}
 		
+//		for(int i=1;i<=10;i++)
+//		{
+//			if(i==5)
+//			{
+//				continue;
+//			}
+//			System.out.println(i);
+//		}
+		
 		for(int i=1;i<=10;i++)
 		{
-			if(i==5)
+			if(i==1 || i==3 || i==5 || i==7 || i==9)
 			{
 				continue;
 			}
